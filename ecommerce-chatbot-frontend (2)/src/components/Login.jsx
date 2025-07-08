b260e2bcaf8fe91f3960b904dff93d29e4163732
SaiManas2106 <158039201+SaiManas2106@users.noreply.github.com>
2025-07-08 18:40:16 +0530

    Update Login.jsx

--- ecommerce-chatbot-frontend (2)/src/components/Login.jsx
+++ ecommerce-chatbot-frontend (2)/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/login", {
+      const res = await axios.post("https://ecommerce-chatbot-application.onrender.com/api/login", {
         username,
         password,
       },{withCredentials:true});
