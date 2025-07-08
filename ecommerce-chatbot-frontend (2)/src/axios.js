431febca640f699a4a5dda5166bd7cce4c49087e
SaiManas2106 <158039201+SaiManas2106@users.noreply.github.com>
2025-07-08 18:42:20 +0530

    Update axios.js

--- ecommerce-chatbot-frontend (2)/src/axios.js
+++ ecommerce-chatbot-frontend (2)/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ecommerce-chatbot-application.onrender.com/api",
   withCredentials: true, // ✅ send session cookie
 });
 
